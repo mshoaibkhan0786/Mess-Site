beb52b77a54c52f69e4970a4b0317e2dcabef29c
Mohammad Shoaib Khan <mshoaibkhan0988@gmail.com>
2025-11-29 12:30:15 +0530

    Fix syntax error in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD-GIU-_XMDNkFNyYpBGfrcuAqypNCboxk",
-    authDomain: "mit-mess-23f98.firebaseapp.com",
-    projectId: "mit-mess-23f98",
-    storageBucket: "mit-mess-23f98.firebasestorage.app",
-    messagingSenderId: "286644195454",
-    appId: "1:286644195454:web:97f456209765291e37082f",
-    measurementId: "G-2TSYYR3ZYM"
-import { initializeApp } from "firebase/app";
+    import { initializeApp } from "firebase/app";
     import { getFirestore } from "firebase/firestore";
     import { getAuth } from "firebase/auth";
     import { getAnalytics } from "firebase/analytics";
