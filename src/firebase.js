923c1544736d8efef5f9dc7f0dcfe829fa89c988
Mohammad Shoaib Khan <mshoaibkhan0988@gmail.com>
2025-11-29 12:31:25 +0530

    Overwrite firebase.js with correct content

--- src/firebase.js
+++ src/firebase.js
@@ -5,25 +5,18 @@ import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD-GIU-_XMDNkFNyYpBGfrcuAqypNCboxk",
-    import { initializeApp } from "firebase/app";
-    import { getFirestore } from "firebase/firestore";
-    import { getAuth } from "firebase/auth";
-    import { getAnalytics } from "firebase/analytics";
+    authDomain: "mit-mess-23f98.firebaseapp.com",
+    projectId: "mit-mess-23f98",
+    storageBucket: "mit-mess-23f98.firebasestorage.app",
+    messagingSenderId: "286644195454",
+    appId: "1:286644195454:web:97f456209765291e37082f",
+    measurementId: "G-2TSYYR3ZYM"
+};
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyD-GIU-_XMDNkFNyYpBGfrcuAqypNCboxk",
-        authDomain: "mit-mess-23f98.firebaseapp.com",
-        projectId: "mit-mess-23f98",
-        storageBucket: "mit-mess-23f98.firebasestorage.app",
-        messagingSenderId: "286644195454",
-        appId: "1:286644195454:web:97f456209765291e37082f",
-        measurementId: "G-2TSYYR3ZYM"
-    };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-    const auth = getAuth(app);
-    const analytics = getAnalytics(app);
-
-    export { db, auth };
+export { db, auth };
